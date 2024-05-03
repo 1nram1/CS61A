c48506ba12468f092e750f49bf1a2786957798db
1nram1 <153077417+1nram1@users.noreply.github.com>
2024-05-04 07:11:58 +0800

    too low efficient

---
+++ 2024exercise/disc/disc01.py
@@ -0,0 +1,115 @@
+def race(x, y):
+    """The tortoise always walks x feet per minute, while the hare repeatedly
+    runs y feet per minute for 5 minutes, then rests for 5 minutes. Return how
+    many minutes pass until the tortoise first catches up to the hare.
+
+    >>> race(5, 7)  # After 7 minutes, both have gone 35 steps
+    7
+    >>> race(2, 4) # After 10 minutes, both have gone 20 steps
+    10
+    """
+    assert y > x and y <= 2 * x, 'the hare must be fast but not too fast'
+    tortoise,hare,minute = 0,0,0
+    while minute == 0 or tortoise < hare :
+        tortoise += x
+        if minute % 10 < 5:
+            hare += y
+        minute += 1
+    return minute
+        # 5min跑后，5min休息
+    
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> print(result)
+    None
+    """
+    "*** YOUR CODE HERE ***"
+    for i in range(1,n + 1,1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else :
+            print(i)
+
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return False
+    for i in range(2,n,1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(101) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    # count = 0
+    # while n > 0:
+    #     current_one = n % 10
+    #     n = n // 10
+    #     if not has_digit(n,current_one) :
+    #         count += 1
+       
+    # return count
+    count = 0
+    for i in range(0,10,1):
+        if has_digit(n,i):
+            count += 1
+    return count
+
+def has_digit(n, k):
+    """Returns whether k is a digit in n.
+
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    assert k >= 0 and k < 10
+    "*** YOUR CODE HERE ***"
+    while n :
+        current_one = n % 10
+        if current_one == k:
+            return True
+        n = n // 10
+    return False
+
+
+    
---
+++ 2024exercise/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << (true && 13) <<endl;
+    return 0;
+}
